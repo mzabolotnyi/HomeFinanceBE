e01b0a9f824cb40aca2a4c1d7cdfd90c5b20d1c6
MZabolotny <mzabolotnyi@gmail.com>
2022-08-10 21:31:00 +0200

    monobank importer

--- src/Service/Transaction/Importer/MonobankImporter.php
+++ src/Service/Transaction/Importer/MonobankImporter.php
@@ -2,6 +2,9 @@
 
 namespace App\Service\Transaction\Importer;
 
+use App\Enum\Currency\CurrencyCode;
+use App\Enum\Transaction\TransactionType;
+use DateTimeImmutable;
 use DateTimeInterface;
 use GuzzleHttp\Client;
 
@@ -16,6 +19,39 @@ class MonobankImporter implements TransactionImporterInterface
 
     public function import(array $options, DateTimeInterface $startDate, DateTimeInterface $endDate): array
     {
-        return [];
+        $token         = $options['apiKey'] ?? null;
+        $uri           = "/personal/statement/0/{$startDate->getTimestamp()}/{$endDate->getTimestamp()}";
+        $response      = $this->client->request('GET', $uri, ['headers' => ['X-Token' => $token]]);
+        $responseItems = json_decode($response->getBody()->getContents(), true);
+        $transactions  = [];
+
+        foreach ($responseItems as $item) {
+            $transactions[] = $this->parseItem($item);
+        }
+
+        return $transactions;
+    }
+
+    private function parseItem(array $item): TransactionModel
+    {
+        $transaction           = new TransactionModel();
+        $transaction->id       = $item['id'];
+        $transaction->date     = (new DateTimeImmutable())->setTimestamp($item['time']);
+        $transaction->type     = $this->parseType($item);
+        $transaction->currency = CurrencyCode::UAH;
+        $transaction->amount   = $this->parseAmount($item);
+        $transaction->comment  = $item['description'];
+
+        return $transaction;
+    }
+
+    private function parseType(array $item): TransactionType
+    {
+        return $item['amount'] > 0 ? TransactionType::Income : TransactionType::Expense;
+    }
+
+    private function parseAmount(array $item): float
+    {
+        return round(abs($item['amount'] / 100), 2);
     }
 }
--- src/Service/Transaction/Importer/TransactionModel.php
+++ src/Service/Transaction/Importer/TransactionModel.php
@@ -2,7 +2,16 @@
 
 namespace App\Service\Transaction\Importer;
 
+use App\Enum\Currency\CurrencyCode;
+use App\Enum\Transaction\TransactionType;
+use DateTimeImmutable;
+
 class TransactionModel
 {
-
+    public string            $id;
+    public DateTimeImmutable $date;
+    public TransactionType   $type;
+    public CurrencyCode      $currency;
+    public float             $amount;
+    public string            $comment;
 }
